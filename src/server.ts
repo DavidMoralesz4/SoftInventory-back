9c46634442571788de8d280c4a30cd528023db16
DavidMoralesz4 <jd3285201@gmail.com>
2024-09-28 01:04:26 -0500

    origin-server

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ const corsOptions = {
   origin: "https://softinventory-back-production.up.railway.app",
   optionsSuccesStatus: 200,
   methods: "GET, POST, PUT, DELETE",// metodos que podre ejecutar en mi front
-  allowedHeaders: 'Content-Type,Authorization' // esto me dara autorizacion al mi front
+  allowedHeaders: 'Content-Type, Authorization' // esto me dara autorizacion al mi front
 };
 
 server.use(cors(corsOptions));
